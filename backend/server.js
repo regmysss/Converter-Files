323b3b579b8a238ff6ae13f042e92c6955417525
Denys <denusdednom5@gmail.com>
2024-05-05 23:47:24 +0300

    refactor: Migrate backend code to ES modules and update dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,126 +1,82 @@
-const express = require('express');
-const cors = require('cors');
-const ffmpeg = require('fluent-ffmpeg');
-let uploads = require('./utils/multer.js');
-const fs = require('fs');
+import express from "express";
+import multer from "multer";
+import cors from "cors";
+import ffmpeg from "fluent-ffmpeg";
+import mime from "mime";
+import fs from "fs";
+
 const app = express();
-const mime = require('mime-types');
+const upload = multer({ dest: "uploads/" });
 
-app.use(cors())
+app.use(cors());
 
-ffmpeg.setFfmpegPath('./ffmpeg/ffmpeg.exe');
-ffmpeg.setFfprobePath('./ffmpeg/ffprobe.exe');
+function reformatFile(file, toFormat) {
+	return new Promise((resolve, reject) => {
+		const mimetype = mime.getType(toFormat);
+		const from = file.mimetype.split("/")[0];
+		let command = ffmpeg(file.path);
 
-function reformatFile(file, toFormat, fromFormat) {
-  return new Promise((res, rej) => {
-    let fmg = ffmpeg(file.path)
+		if (
+			from === "image" ||
+			(from === "video" && mimetype.split("/")[0] === "image")
+		) {
+			command.frames(1);
+		}
 
-    if (fromFormat === 'image') {
-      fmg.frames(1)
-    }
+		if (!fs.existsSync("./outputs")) {
+			fs.mkdirSync("./outputs");
+		}
 
-    if (fromFormat === 'video' && toFormat[1] === 'image') {
-      fmg.frames(1)
-    }
-
-    fmg.save(`./outputs/${file.originalname.split('.')[0]}.${toFormat[0]}`)
-      .on('end', () => {
-        fs.readFile(`./outputs/${file.originalname.split('.')[0]}.${toFormat[0]}`, (err, data) => {
-          if (err) {
-            console.error(err);
-            rej(err);
-            return;
-          }
-
-          res(data.toString('base64'))
-        });
-      })
-      .on("error", (err) => {
-        console.error("Error:", err);
-        rej(err);
-      })
-      .run();
-  })
+		const destination = `./outputs/${file.filename}.${toFormat}`;
+		command
+			.on("end", () => {
+				fs.readFile(destination, (err, data) => {
+					if (err) return reject(err);
+					const base64 = data.toString("base64");
+					resolve({ base64, destination });
+				});
+			})
+			.on("error", (err) => {
+				reject(err);
+			})
+			.save(destination);
+	});
 }
 
-async function waitForFileExists(filePath, currentTime = 0, timeout = 3000) {
-  if (fs.existsSync(filePath)) {
-    return true;
-  }
+app.post("/", upload.array("files"), (req, res) => {
+	const toFormat = req.body.toFormat;
+	const files = req.files;
 
-  if (currentTime === timeout)
-    return false;
+	const response = {};
+	const queue = files.map((file) => {
+		return new Promise((resolve, reject) => {
+			return reformatFile(file, toFormat)
+				.then(({ base64, destination }) => {
+					response[file.originalname] = {
+						base64,
+						mimetype: mime.getType(destination),
+					};
+					fs.unlinkSync(file.path);
+					fs.unlinkSync(destination);
+					resolve();
+				})
+				.catch((err) => {
+					reject(err);
+				});
+		});
+	});
 
-  await new Promise((res) => setTimeout(() => res(), 1000))
-
-  return waitForFileExists(filePath, currentTime + 1000, timeout);
-}
-
-function deleteFile(filePath, currentTime = 0, timeout = 5000) {
-  fs.unlink(filePath, async (err) => {
-    if (err) {
-      if (currentTime == timeout)
-        return console.error('Error deleting file:', err)
-
-      if (err.code == 'EBUSY' || err.code == 'ENOENT') {
-        await new Promise((res) => setTimeout(() => res(), 1000));
-
-        console.log(currentTime)
-
-        return deleteFile(filePath, currentTime + 1000, timeout);
-      }
-    }
-  })
-}
-
-async function clearFiles(files, toFormat) {
-  for (let i = 0; i < files.length; i++) {
-    let uplfile = `./uploads/${files[i].originalname}`
-    let outfile = `./outputs/${files[i].originalname.split('.')[0]}.${toFormat}`
-
-    if (await waitForFileExists(uplfile)) {
-      deleteFile(uplfile);
-    }
-
-    if (await waitForFileExists(outfile)) {
-      deleteFile(outfile);
-    }
-  }
-}
-
-app.post('/', uploads, async function (req, res) {
-  const toFormat = req.body.toFormat.toLowerCase().split(',');
-  const fromFormat = req.body.fromFormat;
-  const files = req.files;
-
-  let json = {};
-  let promises = [];
-
-  files.forEach((file) => {
-    promises.push(new Promise((res, rej) => {
-      reformatFile(file, toFormat, fromFormat).then((base64) => {
-        let fileName = file.originalname.split('.')[0];
-        json[fileName] = { base64data: base64, mimetype: mime.lookup(`./outputs/${fileName}.${toFormat[0]}`) };
-        res();
-      }).catch((err) => rej(err));
-    }))
-  });
-
-
-  Promise.all(promises)
-    .then(() => {
-      res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify(json));
-    })
-    .catch(() => {
-      res.status(500).send("Internal Server Error");
-    })
-    .finally(() => {
-      clearFiles(files, toFormat[0]);
-    })
+	Promise.all(queue)
+		.then(() => {
+			res.json(response);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).send("Internal Server Error");
+		});
 });
 
-
-app.listen(3000, () => {
-  console.log('Server start');
-})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
